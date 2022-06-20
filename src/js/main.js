4509004bd0c29b2792a50abe94f79ac3d03ddef1
blackfxtalon <andyv@inbox.ru>
2022-06-20 15:15:12 +0500

    start project; ready header

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+/* alpine.js */
+
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+ 
+Alpine.start()
+
+// alpine params
+
+/* swiper.js */
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+// custom scripts
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+});
